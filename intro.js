2cf277ec3f03efa1c00f9d221f82625e80f0362a
Jose Pita <josepita@trevenque.es>
2021-01-25 20:29:30 +0100

    añado cositas

---
+++ intro.js
@@ -0,0 +1,16 @@
+//Creando variables
+
+//Forma 1 - Var
+var texto = "este es mi texto";
+var numero = 97;
+
+//Forma 2 - Let (ES6)
+let texto2 = {};
+
+//Forma 3 - Cons (ES6)
+const q = "nuevo texto";
+
+//let y const funcionan en ámbito de bloque 
+//es decir, si se definen en un bloque no salen de su ámbito (lo que esté entre las llaves)
+
+console.log(q);
